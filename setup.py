aff083e048af7dad1a7f544c41d48dbb1371d24e
Christian Kohout <ck@getaweb.at>
2014-07-24 08:52:42 +0200

    raised version: v0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='djangocms-getaweb-news',
-    version='0.1',
+    version='0.2',
     packages=['djangocms_news'],
     include_package_data=True,
     license='Unlicense',  # example license
