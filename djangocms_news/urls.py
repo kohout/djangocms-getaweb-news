eaec46e96accd7be645f49fa5e42d8fb0e5680ff
Christian Kohout <ck@getaweb.at>
2014-11-25 16:15:31 +0100

    fixed issue with url pattern which was not strict enough

--- djangocms_news/urls.py
+++ djangocms_news/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from .views import NewsListView, NewsDetailView
 
 urlpatterns = patterns('',
-    url(r'^(?P<slug>[\w-]+)/$',
+    url(r'^news/(?P<slug>[\w-]+)/$',
         NewsDetailView.as_view(),
         name='news-detail'),
     url(r'^$',
