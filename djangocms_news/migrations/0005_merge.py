d122619c18602b4da06ed5284724460db7b5b3fb
Daniel Mahlberg <dm@getaweb.at>
2016-04-14 16:09:30 +0200

    Merged conflicting migrations

---
+++ djangocms_news/migrations/0005_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('djangocms_news', '0004_auto_20160413_1622'),
+        ('djangocms_news', '0004_auto_20160414_1339'),
+    ]
+
+    operations = [
+    ]
